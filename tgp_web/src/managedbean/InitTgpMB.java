1599e0f2aef249820d3ea366b8111e4cec089c6e
Claudio <Claudio@pca>
2015-05-23 19:34:28 -0300

    sss

--- tgp_web/src/managedbean/InitTgpMB.java
+++ tgp_web/src/managedbean/InitTgpMB.java
@@ -1,9 +1,11 @@
 package managedbean;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -18,9 +20,13 @@ import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.primefaces.event.FileUploadEvent;
 
 import model.Atividade;
 import model.ConfigAtividade;
@@ -66,6 +72,8 @@ public class InitTgpMB  implements Serializable {
 	private List<UsuarioAtividade> usuarioAtividades = new ArrayList<UsuarioAtividade>();
 	private UsuarioAtividade usuarioAtividadeAtual = new UsuarioAtividade();
 	private ConfigAtividade configAtividadeAtual = new ConfigAtividade();
+	private DocAtividade docAtividade = new DocAtividade();
+	private List<DocAtividade> docsAtividades = new ArrayList<DocAtividade>();
 	
 	
 	public InitTgpMB() {
@@ -110,6 +118,8 @@ public class InitTgpMB  implements Serializable {
 		this.atividadeAtual = new Atividade();
 		this.usuarioAtividadeAtual = new UsuarioAtividade();
 		this.configAtividadeAtual = new ConfigAtividade();
+		this.docAtividade = new DocAtividade();
+		 docsAtividades = new ArrayList<DocAtividade>();
 		
 		
 	}
@@ -290,9 +300,10 @@ public class InitTgpMB  implements Serializable {
 	
 	public void initDoc(Atividade ativ){
 		
-		FacesContext fc = FacesContext.getCurrentInstance();
-		HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
-		session.setAttribute("ativId", ativ.getAtividadeId());
+		this.atividadeAtual =  atividadeFacade.find(ativ.getAtividadeId());
+		this.atividadeAtual.setDocAtividades(atividadeFacade.findDocAtividade(this.atividadeAtual.getAtividadeId()));
+		this.docAtividade = new DocAtividade();
+		this.docsAtividades = this.atividadeAtual.getDocAtividades();
 	}
 	
 	
@@ -307,6 +318,115 @@ public class InitTgpMB  implements Serializable {
 		FacesContext.getCurrentInstance().addMessage(null,	new FacesMessage(FacesMessage.SEVERITY_INFO,  info, ""));
 	}
 	
+	
+	
+	public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        this.docAtividade.setDoc(event.getFile().getContents());
+        this.docAtividade.setTypeName(event.getFile().getContentType());
+        String extensao = event.getFile().getFileName().substring(event.getFile().getFileName().lastIndexOf('.') + 1); 
+        this.docAtividade.setExtensao("."+extensao);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+	
+	
+	public void salvarDoc(){
+		
+		if (docAtividade.getDoc() != null){
+			DocAtividade docAtividade = this.docAtividade;
+			
+			if (docAtividade.getNomeDoc() == null || docAtividade.getNomeDoc().equals("")){
+				String info = "Informe nome do documento.";
+				FacesContext.getCurrentInstance().addMessage(null,	new FacesMessage(FacesMessage.SEVERITY_ERROR,info, ""));
+			}
+			
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
+			Usuario usuario = (Usuario) session.getAttribute("user");
+			
+			if (usuario != null){
+				
+				docAtividade.setUsuarioAtualizador(usuario.getNome());
+			}
+			
+			this.docAtividade.setDataInsert(new Date());
+			
+			
+			
+			this.docAtividade.setAtividade(this.atividadeAtual);
+			
+			docAtividadeFacade.save(this.docAtividade);
+			
+			this.docsAtividades.add(this.docAtividade);
+			
+			this.docAtividade = new DocAtividade();
+			
+			String info = "Doc. carregado com sucesso.";
+			FacesContext.getCurrentInstance().addMessage(null,	new FacesMessage(FacesMessage.SEVERITY_INFO,info, ""));
+		} else {
+			String info = "Carregue pelo menos um documento.";
+			FacesContext.getCurrentInstance().addMessage(null,	new FacesMessage(FacesMessage.SEVERITY_ERROR,info, ""));
+		}
+		
+	}
+	
+	
+	public void downloadDoc(DocAtividade docAtividade) {
+
+		ServletContext sContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+		String arquivo = sContext.getRealPath("/temp") + File.separator	+ docAtividade.getNomeDoc() + docAtividade.getExtensao();
+
+		File file = new File(arquivo);
+
+		try {
+			FileOutputStream fileOuputStream = new FileOutputStream(file);
+			fileOuputStream.write(docAtividade.getDoc());
+			fileOuputStream.close();
+
+			System.out.println("Done");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String fileName = (docAtividade.getNomeDoc() + docAtividade.getExtensao());
+		
+		this.downloadFile(fileName, file, docAtividade.getTypeName(), FacesContext.getCurrentInstance());
+		
+	}
+
+	public  void downloadFile(String filename, File file,String mimeType, FacesContext facesContext) {
+		FileInputStream in = null;
+		try {
+			ExternalContext context = facesContext.getExternalContext();
+			System.out.println("Aqui para baixar o " + filename);
+			HttpServletResponse response = (HttpServletResponse) context
+					.getResponse();
+			response.setHeader("Content-Disposition", "attachment;filename=\""
+					+ filename + "\"");
+			response.setContentLength((int) file.length());
+			response.setContentType(mimeType);
+			in = new FileInputStream(file);
+			OutputStream out = response.getOutputStream();
+			byte[] buf = new byte[(int) file.length()];
+			int count;
+			while ((count = in.read(buf)) >= 0) {
+				out.write(buf, 0, count);
+			}
+			facesContext.responseComplete();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	
+	public void excluirDoc(DocAtividade docAtividade) {
+		this.docsAtividades.remove(docAtividade);
+		docAtividadeFacade.delete(docAtividadeFacade.find(docAtividade
+				.getDocAtividadeId()));
+		String info = "Doc Atividade Excluido com Sucesso";
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, info, null));
+	}
 	
 	
 	
@@ -469,6 +589,37 @@ public class InitTgpMB  implements Serializable {
 
 	public void setConfigAtividadeAtual(ConfigAtividade configAtividadeAtual) {
 		this.configAtividadeAtual = configAtividadeAtual;
+	}
+
+
+	public UsuarioAtividadeFacade getUsuarioAtividadeFacade() {
+		return usuarioAtividadeFacade;
+	}
+
+
+	public void setUsuarioAtividadeFacade(
+			UsuarioAtividadeFacade usuarioAtividadeFacade) {
+		this.usuarioAtividadeFacade = usuarioAtividadeFacade;
+	}
+
+
+	public DocAtividade getDocAtividade() {
+		return docAtividade;
+	}
+
+
+	public void setDocAtividade(DocAtividade docAtividade) {
+		this.docAtividade = docAtividade;
+	}
+
+
+	public List<DocAtividade> getDocsAtividades() {
+		return docsAtividades;
+	}
+
+
+	public void setDocsAtividades(List<DocAtividade> docsAtividades) {
+		this.docsAtividades = docsAtividades;
 	}
 
 
